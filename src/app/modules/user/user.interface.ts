a81bffd23c242ec06f917bd25561e48c9ab6a8e3
f4faysal <f4faysals@gmail.com>
2023-10-06 12:53:25 +0600

    Projact init

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+import { Model, ObjectId, Types } from 'mongoose';
+
+export type IUser = {
+  _id?: ObjectId;
+  id: string;
+  role: 'Admin' | 'RentUser' | 'HomeOwner';
+  password: string;
+  nidNumber: number;
+  nidVerified: boolean;
+  needsPasswordChange: boolean;
+  admin?: Types.ObjectId;
+  rentUser?: Types.ObjectId;
+  homeOwner?: Types.ObjectId;
+};
+
+// export type IUserMethods = {
+//   isUserExist(id: string): Promise<Partial<IUser> | null>;
+//   isPasswordMatched(
+//     givenPassword: string,
+//     savedPassword: string
+//   ): Promise<boolean>;
+// };
+
+export type UserModel = {
+  isUserExist(
+    id: string
+  ): Promise<Pick<IUser, 'id' | 'password' | 'role' | 'needsPasswordChange'>>;
+  isPasswordMatched(
+    givenPassword: string,
+    savedPassword: string
+  ): Promise<boolean>;
+} & Model<IUser>;
+
+// export type UserModel = Model<IUser, Record<string, unknown>, IUserMethods>;
