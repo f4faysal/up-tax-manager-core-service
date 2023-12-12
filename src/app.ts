a25e3a4206181d569ac8ed828ccc5ed8ed227b71
f4faysal <f4faysals@gmail.com>
2023-12-13 01:54:13 +0600

    Update CORS origin in app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://localhost:5000'], // allow to server to accept request from different origin
+    origin: ['http://localhost:3000', 'https://locat-tax.vercel.app'], // allow to server to accept request from different origin
     credentials: true,
   })
 );
